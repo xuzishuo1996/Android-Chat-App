edf8f95754a79a68fbd9476d2ed60b8c8132b2a5
Zishuo Xu <xuzishuo1996@gmail.com>
2021-06-20 12:59:54 -0400

    design sign up activity

---
+++ app/src/main/java/com/company/mychatapp/SignUpActivity.java
@@ -0,0 +1,29 @@
+package com.company.mychatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private CircleImageView imageViewCircle;
+    private TextInputEditText editTextEmailSignup,editTextPasswordSignup,editTextUserNameSignup;
+    private Button buttonRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        imageViewCircle = findViewById(R.id.imageViewCircle);
+        editTextEmailSignup = findViewById(R.id.editTextEmailSignup);
+        editTextPasswordSignup = findViewById(R.id.editTextPasswordSignup);
+        editTextUserNameSignup = findViewById(R.id.editTextUserNameSignup);
+        buttonRegister = findViewById(R.id.buttonRegister);
+    }
+}
