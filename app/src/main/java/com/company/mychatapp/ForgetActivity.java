fadd9f607604128870bf07ce6ff6ccb12d90bb72
Zishuo Xu <xuzishuo1996@gmail.com>
2021-06-20 13:03:50 -0400

    add forget password activity

---
+++ app/src/main/java/com/company/mychatapp/ForgetActivity.java
@@ -0,0 +1,23 @@
+package com.company.mychatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class ForgetActivity extends AppCompatActivity {
+
+    private TextInputEditText editTextForget;
+    private Button buttonForget;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget);
+
+        editTextForget = findViewById(R.id.editTextForget);
+        buttonForget = findViewById(R.id.buttonForget);
+    }
+}
